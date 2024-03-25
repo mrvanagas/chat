4026bcfe2c3795ee0cb7ba9ea33c9b2824d8980b
Lukas <lukonelis1@gmail.com>
2024-03-25 14:03:33 +0200

    refactored backend code, now separated to separate modules

--- chat-app-be/server.js
+++ chat-app-be/server.js
@@ -1,30 +1,16 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
-
 const http = require('http');
-const { Server } = require('socket.io');
-
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*", // Adjust this to match your client's URL
-    methods: ["GET", "POST"],
-  }
-});
+const { setupWebSocket } = require('./services/socket')
+const connectDB = require('./config/db')
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-
-  socket.on('chat message', (msg) => {
-    console.log('Message Received: ', msg);
-    io.emit('chat message', msg);
-  });
-});
+connectDB(); //Connect to DB
+setupWebSocket(server); // Initialize WebSocket
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
